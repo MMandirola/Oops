8ae1a5171c8b4667a94bf6fe6b3bed4043ab8c21
marcelo <marcelo@marcelo.marcelo>
2017-10-31 20:38:18 -0300

    Boolean

--- src/OopsBoolean.java
+++ src/OopsBoolean.java
@@ -1,7 +1,20 @@
 
 public class OopsBoolean extends OopsObject{
+	private boolean value;
+	public boolean isValue() {
+		return value;
+	}
+	public OopsBoolean(boolean value) {
+		this.value = value;
+	}
+	public void setValue(boolean value) {
+		this.value = value;
+	}
 	public OopsObject respond(String name, OopsObject[] args) {
-		// TODO Auto-generated method stub
+//		switch{
+//			case "!":
+//				return OopsBoolean()
+//		}
 		return null;
 	}
 
