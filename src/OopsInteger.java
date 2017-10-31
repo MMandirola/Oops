2be87bbd0cf9a8ac284a7b6e2e25d6be70e1d429
dimartiro <diego2737@gmail.com>
2017-10-31 20:39:08 -0300

    Adds OopsInteger

--- src/OopsInteger.java
+++ src/OopsInteger.java
@@ -1,9 +1,40 @@
 
 public class OopsInteger extends OopsObject{
 
+	Integer value;
+	
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+
+	public OopsInteger(Integer value){
+		this.value = value;
+	}
+
 	public OopsObject respond(String name, OopsObject[] args) {
-		// TODO Auto-generated method stub
-		return null;
+		OopsObject result = null;
+		OopsInteger firstArg = (OopsInteger) args[0];
+		
+		switch(name){
+			case "+":
+				result = new OopsInteger(this.value + firstArg.getValue());
+			case "-":
+				result = new OopsInteger(this.value - firstArg.getValue());
+			case "*":
+				result = new OopsInteger(this.value * firstArg.getValue());
+			case "<=":
+				result = new OopsBoolean(this.value <= firstArg.getValue());
+			case "==":
+				result = new OopsBoolean(this.value == firstArg.getValue());
+			case "!=":
+				result = new OopsBoolean(this.value != firstArg.getValue());
+		}
+		
+		return result;
 	}
 	
 }
