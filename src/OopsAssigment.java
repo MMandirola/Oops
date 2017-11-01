c192d5d0b7e13b7850922f0a9ad119da954d905f
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-10-31 22:20:40 -0300

    add assigment

---
+++ src/OopsAssigment.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author emanuel
+ */
+public class OopsAssigment extends OopsCode{
+    OopsVariable var;
+    OopsCode obj;
+    
+    public OopsAssigment(OopsVariable xvar, OopsCode xobj){
+        this.var = xvar;
+        this.obj = xobj;
+    }
+
+    @Override
+    public OopsObject evaluate(OopsState state) {
+        
+       OopsObject res = obj.evaluate(state);
+       return state.state.put(var.getVar(), res);
+    }
+    
+    
+}
